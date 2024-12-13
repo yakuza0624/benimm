30a568ad52f61beda1a6e9e998f0c4d9a64f68f0
yakuza0624 <ismaildaglitr@gmail.com>
2024-12-13 17:44:37 +0500

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,26 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('rusca-kelime-v1').then(function(cache) {
-            return cache.addAll([
-                './',
-                './index.html',
-                './manifest.json',
-                './icon-192.png',
-                './icon-512.png'
-            ]);
-        })
-    );
+const CACHE_NAME = 'rusca-kelime-cache-v1';
+const urlsToCache = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
 });
