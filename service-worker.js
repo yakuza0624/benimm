f27779513f13fff34f6588caa217c2a3bcc87eca
yakuza0624 <ismaildaglitr@gmail.com>
2024-12-13 17:12:16 +0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('rusca-kelime-v1').then(function(cache) {
+            return cache.addAll([
+                './',
+                './index.html',
+                './manifest.json',
+                './icon-192.png',
+                './icon-512.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
